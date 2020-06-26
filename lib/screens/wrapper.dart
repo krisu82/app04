f353c71a5bed2618c8dbe3e837f56986365b637a
krisu82 <krisu.82@gmail.com>
2020-06-26 15:53:05 +0200

    lesson02

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:app04/screens/authenticate/authenticate.dart';
+import 'package:app04/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    
+    //zwrotka home lub aunteticate widget
+    return Authenticate();
+  }
+}
