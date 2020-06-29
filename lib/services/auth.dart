26957fe7904dcc4588b1a11bcea3ad1ca81ee1c5
krisu82 <krisu.82@gmail.com>
2020-06-29 15:30:44 +0200

    lesson 6

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,15 +1,24 @@
+import 'package:app04/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 
 class AuthService {
+
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+// tworze user obj bazujac na firebaseUser
+
+User _userFromFirebaseUser(FirebaseUser user) {
+return user != null ? User (uid: user.uid) : null;
+}
 
 // sign in anonymous
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
+
     } catch (e) {
       print(e.toString());
       return null;
